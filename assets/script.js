532da0cd85f391ac1f901bc59783ab4d506c4553
moaaskt <moacirmayer@hotmail.com>
2023-07-31 17:08:25 -0300

    att css

--- assets/script.js
+++ assets/script.js
@@ -68,47 +68,47 @@ nextPageButton.addEventListener("click", () => {
 // funçao para procurar personagem
 
 const searchCharacter = async () => {
-    const searchInput = document.querySelector("#searchInput");
-    const characterName = searchInput.value.trim();
-  
-    if (characterName === "") {
-      alert("Por favor, digite o nome de um personagem.");
-      return;
-    }
-  
-    const url = `https://rickandmortyapi.com/api/character/?name=${characterName}`;
-    const api = await fetch(url);
-    const data = await api.json();
-    console.log(data);
-    divRes = document.querySelector("#resultado");
-    divRes.innerHTML = "";
-  
-    if (data.error) {
-      alert("Personagem não encontrado. Tente novamente.");
-      return;
+  const searchInput = document.querySelector("#searchInput");
+  const characterName = searchInput.value.trim();
 
-      
+  if (characterName === "") {
+    alert("Por favor, digite o nome de um personagem.");
+    return;
+  }
 
-      
-    }
-  
-    data.results.forEach((item) => {
-      const divItem = document.createElement("div");
-      divItem.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <img src="${item.image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${item.name}</h5>
-            <p class="card-text"><b>Status: </b>${item.status}</p>
-            <p class="card-text"><b>Espécie: </b>${item.species}</p>
-            <p class="card-text"><b>Genero: </b>${item.gender}</p>
-          </div>
+  const url = `https://rickandmortyapi.com/api/character/?name=${characterName}`;
+  const api = await fetch(url);
+  const data = await api.json();
+  console.log(data);
+  divRes = document.querySelector("#resultado");
+  divRes.innerHTML = "";
+
+  if (data.error) {
+    alert("Personagem não encontrado. Tente novamente.");
+    // Limpar o campo de busca
+    searchInput.value = "";
+    // Voltar à exibição dos personagens iniciais
+    apiRick(1);
+    return;
+  }
+
+  data.results.forEach((item) => {
+    const divItem = document.createElement("div");
+    divItem.innerHTML = `
+      <div class="card" >
+        <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text"><b>Status: </b>${item.status}</p>
+          <p class="card-text"><b>Espécie: </b>${item.species}</p>
+          <p class="card-text"><b>Genero: </b>${item.gender}</p>
         </div>
-      `;
-  
-      divRes.appendChild(divItem);
-    });
-  };
+      </div>
+    `;
+
+    divRes.appendChild(divItem);
+  });
+};
 
 
 
@@ -129,7 +129,7 @@ const searchCharacter = async () => {
     data.results.forEach((item) => {
       const divItem = document.createElement("div");
       divItem.innerHTML = `
-        <div class="card" style="width: 18rem;">
+        <div class="card" >
           <img src="${item.image}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${item.name}</h5>
