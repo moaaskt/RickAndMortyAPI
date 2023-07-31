92db712c77d1b10b78efda0b0478e47de10d7c0f
moaaskt <moacirmayer@hotmail.com>
2023-07-31 16:40:45 -0300

    atualizando pagination

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,71 @@
+// Variável para guardar o número total de páginas
+let totalPages = 1;
+let currentPage = 1;
+
+// Função para exibir os personagens da página especificada
 const apiRick = async (page) => {
+  try {
     let url = `https://rickandmortyapi.com/api/character/?page=${page}`;
     const api = await fetch(url);
     const data = await api.json();
     console.log(data);
-    divRes = document.querySelector("#resultado")
-    divRes.innerHTML =""
-    data.results.map(item => {
-      divItem = document.createElement('div');
-      divItem.classList.add('card'); // Adiciona a classe "card" ao elemento
+    divRes = document.querySelector("#resultado");
+    divRes.innerHTML = "";
+    data.results.forEach((item) => {
+      const divItem = document.createElement("div");
+      divItem.classList.add("card");
       divItem.innerHTML = `
-          <img src="${item.image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${item.name}</h5>
-            <p class="card-text"><b>Status: </b>${item.status}</p>
-            <p class="card-text"><b>Espécie: </b>${item.species}</p>
-            <p class="card-text"><b>Gênero: </b>${item.gender}</p>
-          </div>
+        <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text"><b>Status: </b>${item.status}</p>
+          <p class="card-text"><b>Espécie: </b>${item.species}</p>
+          <p class="card-text"><b>Gênero: </b>${item.gender}</p>
+        </div>
       `;
-  
-      divItem.addEventListener('click', () => {
+      divItem.addEventListener("click", () => {
         showCharacterDetails(item.id);
       });
       divRes.appendChild(divItem);
-  })
-  
-}
+    });
 
-// ... (código existente) ...
+    // Atualizar o número total de páginas e a página atual
+    totalPages = data.info.pages;
+    currentPage = page;
+
+    // Habilitar/desabilitar os botões de página anterior e próxima
+    const prevPageButton = document.querySelector("#prevPageButton");
+    const nextPageButton = document.querySelector("#nextPageButton");
+    prevPageButton.disabled = currentPage === 1;
+    nextPageButton.disabled = currentPage === totalPages;
+  } catch (error) {
+    console.error("Erro ao obter os personagens:", error);
+    window.alert("Ocorreu um erro ao obter os personagens. Tente novamente mais tarde.");
+  }
+};
+
+// Event listener para o botão de página anterior
+const prevPageButton = document.querySelector("#prevPageButton");
+prevPageButton.addEventListener("click", () => {
+  if (currentPage > 1) {
+    apiRick(currentPage - 1);
+  }
+});
+
+// Event listener para o botão de próxima página
+const nextPageButton = document.querySelector("#nextPageButton");
+nextPageButton.addEventListener("click", () => {
+  if (currentPage < totalPages) {
+    apiRick(currentPage + 1);
+  }
+});
+
+// Restante do seu código...
+
+// Restante do seu código...
+
+
+// funçao para procurar personagem
 
 const searchCharacter = async () => {
     const searchInput = document.querySelector("#searchInput");
@@ -71,6 +110,9 @@ const searchCharacter = async () => {
     });
   };
 
+
+
+  // funçao para filtrar os personagens "vivo,morto ou desconhecido"
   const filterCharacters = async (status) => {
     const url = `https://rickandmortyapi.com/api/character/?status=${status}`;
     const api = await fetch(url);
@@ -97,7 +139,9 @@ const searchCharacter = async () => {
           </div>
         </div>
       `;
-  
+      divItem.addEventListener("click", () => {
+        showCharacterDetails(item.id);
+      }); 
       divRes.appendChild(divItem);
     });
   
@@ -111,7 +155,11 @@ const searchCharacter = async () => {
     const selectedButton = document.querySelector(`.btn-filter.${status.toLowerCase()}`);
     selectedButton.classList.add("active");
   };
+  
 
+
+
+  // função modal com informaçoes dos personagens
   const showCharacterDetails = async (characterId) => {
     try {
       const response = await fetch(
@@ -119,15 +167,7 @@ const searchCharacter = async () => {
       );
       const characterData = await response.json();
   
-      // Verificar se a modal existe antes de preencher o conteúdo
-      const characterModal = document.querySelector("#characterModal");
-      const characterModalBody = document.querySelector("#characterModalBody");
-      if (!characterModal || !characterModalBody) {
-        console.error("Modal não encontrada.");
-        return;
-      }
-  
-      // Montar o conteúdo da modal com as informações do personagem
+      // Preencher o conteúdo da modal com as informações do personagem
       let modalContent = `
         <p><b>Nome: </b>${characterData.name}</p>
         <p><b>Status: </b>${characterData.status}</p>
@@ -138,11 +178,12 @@ const searchCharacter = async () => {
       `;
   
       // Preencher o conteúdo da modal com as informações do personagem
+      const characterModalBody = document.querySelector("#characterModalBody");
       characterModalBody.innerHTML = modalContent;
   
-      // Exibir a modal usando o Bootstrap
-      const bootstrapModal = new bootstrap.Modal(characterModal);
-      bootstrapModal.show();
+      // Abrir a modal usando o método show
+      const characterModal = new bootstrap.Modal(document.getElementById("characterModal"));
+      characterModal.show();
     } catch (error) {
       console.error("Erro ao obter os detalhes do personagem:", error);
       window.alert(
@@ -150,6 +191,19 @@ const searchCharacter = async () => {
       );
     }
   };
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    // Esta função será chamada quando o conteúdo da página for carregado
+  
+    // Chame a função apiRick com a página inicial (1)
+    apiRick(1);
+  });
+  
+  
+  
+  
+  
+  
   
   
 
