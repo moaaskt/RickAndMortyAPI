77711fbc4993a101e56630f0baa30c909d3b7a88
moaaskt <moacirmayer@hotmail.com>
2023-07-31 03:27:19 -0300

    Edit css

--- assets/script.js
+++ assets/script.js
@@ -67,8 +67,51 @@ const searchCharacter = async () => {
       divRes.appendChild(divItem);
     });
   };
+
+  const filterCharacters = async (status) => {
+    const url = `https://rickandmortyapi.com/api/character/?status=${status}`;
+    const api = await fetch(url);
+    const data = await api.json();
+    console.log(data);
+    divRes = document.querySelector("#resultado");
+    divRes.innerHTML = "";
+  
+    if (data.error) {
+      alert("Nenhum personagem encontrado com esse status.");
+      return;
+    }
+  
+    data.results.forEach((item) => {
+      const divItem = document.createElement("div");
+      divItem.innerHTML = `
+        <div class="card" style="width: 18rem;">
+          <img src="${item.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <p class="card-text"><b>Status: </b>${item.status}</p>
+            <p class="card-text"><b>Espécie: </b>${item.species}</p>
+            <p class="card-text"><b>Genero: </b>${item.gender}</p>
+          </div>
+        </div>
+      `;
+  
+      divRes.appendChild(divItem);
+    });
+  
+   
+    const filterButtons = document.querySelectorAll(".btn-filter");
+    filterButtons.forEach((button) => {
+      button.classList.remove("active");
+    });
+  
+   
+    const selectedButton = document.querySelector(`.btn-filter.${status.toLowerCase()}`);
+    selectedButton.classList.add("active");
+  };
+
   
 
 
 
 apiRick(1)
+
